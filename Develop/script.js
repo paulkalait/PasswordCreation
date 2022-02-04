aa5c71aad8b83571f70d7ef086e6348d3587d120
Your Name <kalaitzidispaul@gmail.com>
2022-02-04 13:51:17 -0500

    started document

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,19 @@
 // Assignment code here
+document.querySelector('#generate').addEventListener("click", randomPassword);
 
+//Define Arrarys to fullfill all the options
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = password; 
 
 }
 
